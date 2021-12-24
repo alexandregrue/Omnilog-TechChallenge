f3f825f4321c8acd3479c975c6aed702aa951b0b
Alexandre Grué <alexandre.grue@hotmail.com>
2021-12-24 15:34:21 +0100

    make verification for size of file and his extension

--- src/Form/NoteType.php
+++ src/Form/NoteType.php
@@ -11,6 +11,8 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\File;
 
 class NoteType extends AbstractType
 {
@@ -40,6 +42,22 @@ class NoteType extends AbstractType
                 'multiple' => true,
                 'mapped' => false,
                 'required' => false,
+                'constraints' => [
+                    new All([
+                        new File([
+                            'maxSize' => '2M',
+                            'uploadIniSizeErrorMessage' => 'Le fichier doit faire moins de 2Mo.',
+                            'mimeTypes' => [
+                                'image/png',
+                                'image/jpg',
+                                'image/jpeg',
+                                'application/pdf',
+                                'application/x-pdf',
+                            ],
+                            'mimeTypesMessage' => 'Merci de télécharger un fichier au format PNG, JPG, JPEG ou PDF.',
+                        ]),
+                    ]),
+                ],
             ])
         ;
     }
