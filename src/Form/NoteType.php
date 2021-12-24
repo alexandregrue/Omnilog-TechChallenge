13546eb2c6c81313849d0ba91e105723aa71ddc7
Alexandre Grué <alexandre.grue@hotmail.com>
2021-12-24 14:29:08 +0100

    make entrance control on note

--- src/Form/NoteType.php
+++ src/Form/NoteType.php
@@ -6,7 +6,9 @@ namespace App\Form;
 
 use App\Entity\Note;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,10 +17,24 @@ class NoteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('text')
-            ->add('startDate')
-            ->add('endDate')
+            ->add('title', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('text', TextType::class, [
+                'label' => 'Description',
+            ])
+            ->add('startDate', DateType::class, [
+                'label' => 'Début',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'invalid_message' => 'La date doit être sous la forme JJ/MM/AAAA',
+            ])
+            ->add('endDate', DateType::class, [
+                'label' => 'Fin',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'invalid_message' => 'La date doit être sous la forme JJ/MM/AAAA',
+            ])
             ->add('documents', FileType::class, [
                 'label' => 'Documents',
                 'multiple' => true,
