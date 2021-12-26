daf0416d72cd02e6e81849f86f55d978c78dbdd4
Alexandre Grué <alexandre.grue@hotmail.com>
2021-12-26 10:23:15 +0100

    verify not found formatting files and html verify

--- public/js/documents.js
+++ public/js/documents.js
@@ -1,28 +1,29 @@
 window.onload = () => {
     //select delete button
-    let links = document.querySelectorAll('[data-delete]');
-    for(link of links){
-        link.addEventListener('click', function(e){
+    let links = document.querySelectorAll("[data-delete]");
+    for (link of links) {
+        link.addEventListener("click", function (e) {
             e.preventDefault();
-            if(confirm('Voulez-vous vraiment supprimer ce document?')){
+            if (confirm("Voulez-vous vraiment supprimer ce document?")) {
                 // Ajax request to href with DELETE method
-                fetch(this.getAttribute('href'), {
-                    method: 'DELETE',
+                fetch(this.getAttribute("href"), {
+                    method: "DELETE",
                     headers: {
-                        'X-Requested-With': 'XMLHttpRequest',
-                        'Content-Type': 'application/json',
+                        "X-Requested-With": "XMLHttpRequest",
+                        "Content-Type": "application/json",
                     },
-                    body: JSON.stringify({"_token": this.dataset.token}),
-                }).then(
-                    // Get JSON response
-                    response => response.json()
-                ).then(data => {
-                    if(data.success)
-                        this.parentElement.remove();
-                    else
-                        alert(data.error);
-                }).catch(e => alert(e));
+                    body: JSON.stringify({ _token: this.dataset.token }),
+                })
+                    .then(
+                        // Get JSON response
+                        (response) => response.json()
+                    )
+                    .then((data) => {
+                        if (data.success) this.parentElement.remove();
+                        else alert(data.error);
+                    })
+                    .catch((e) => alert(e));
             }
         });
     }
-}
+};
